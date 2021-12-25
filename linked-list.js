9a655e88cfd701aec7a94bfe5e26b9f8f3dedf94
Virginia Wu <virginiawu11@gmail.com>
2021-12-24 22:19:57 -0500

    complete unshift

--- linked-list.js
+++ linked-list.js
@@ -34,7 +34,17 @@ class LinkedList {
 
   /** unshift(val): add new value to start of list. */
 
-  unshift(val) {}
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
 
   /** pop(): return & remove last item. */
 
